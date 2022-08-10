380ad175d744e16f56a0311799609122fa667720
Marcus <marcusicaromc@gmail.com>
2022-08-10 10:48:24 -0300

    Write computer choice code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,19 @@
 //Create a function that will return a computer generated value between 1, 2 and 3
-// Each one of those values will be equivalent to one of these: rock (1), paper (2), scissor (3)
+// Each one of those values will be equivalent to one of these: rock (0), paper (1), scissor (2)
 function getComputerChoice(){
     var value = Math.floor(Math.random() * 3);
-    return value;
+    if (value === 0){
+        return "Rock";
+    } 
+
+    else if (value === 1){
+        return "Paper";
+    }
+
+    else {
+        return "Scissor";
+    }
 }
+
 
 console.log(getComputerChoice());
