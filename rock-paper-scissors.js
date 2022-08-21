7ec9f96263c64db8cc7318fed025d24147b4f8d3
Marcus <marcusicaromc@gmail.com>
2022-08-21 17:11:11 -0300

    Create UI interface.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,10 +1,11 @@
-//Create a function that will return a computer generated value between 1, 2 and 3
-// Each one of those values will be equivalent to one of these: rock (0), paper (1), scissor (2)
 
 var rock = "Rock";
 var paper = "Paper";
 var scissors = "Scissors";
 let score = 0;
+var computerScore = 0;
+var rounds = 0;
+var matches = 0;
 
 
 function getComputerChoice(){
@@ -26,55 +27,114 @@ function getComputerChoice(){
 
 }
 
-//Create a function that receives the player input 
-//turn it into lower case
-//return "rock", "paper" or "scissor" based on what the player selected
 
-var playerInput = prompt("Please, select one of these: rock, paper or scissor.");
 
-var lowerCasePlayerInput = playerInput.toLowerCase();
+var playerInput = '';
 
-//Create a function that takes both the player input and the computer choice
-//Evaluates them and return a result based on rock, paper, scissors rules
+var lowerCasePlayerInput = '';
+
+function scoreRounds(){
+    scoreSelector.textContent = 'Current score: ' + score;
+    if(score > computerScore && rounds == 5) {
+        matches ++;
+        winnerSelector.textContent = 'Game Over. You won ' + score + ' rounds and the computer won ' + computerScore + ' rounds. You won.';
+        matchesPlayed.textContent = matches + ' matches played.';
+        return score = 0, computerScore = 0, rounds = 0;
+    }
+
+    else if(score < computerScore && rounds == 5) {
+        matches ++;
+        winnerSelector.textContent = 'Game Over. You won ' + score + ' rounds and the computer won ' + computerScore + ' rounds. You lost.';
+        matchesPlayed.textContent = matches + ' matches played.';
+        return score = 0, computerScore = 0, rounds = 0;
+    }
+
+    else if(score == computerScore && rounds == 5){
+        matches ++;
+        winnerSelector.textContent = 'Game Over. You won ' + score + ' rounds and the computer won ' + computerScore + ' rounds. It\'s a tie.';
+        matchesPlayed.textContent = matches + ' matches played.';
+        return score = 0, computerScore = 0, rounds = 0;
+    }
+}
+
+
 
 function playRound(lowerCasePlayerInput, getComputerChoice){
     
     if (lowerCasePlayerInput === 'rock' && getComputerChoice === 'Rock'){
+        //console.log('Draw');
+        rounds ++;
+        resultsSelector.textContent = 'You chose rock and the computer chose rock. Draw!';
+        scoreRounds();
         return "Draw";        
     }
 
     else if (lowerCasePlayerInput === 'rock' && getComputerChoice === 'Paper'){
+        //console.log('You lose');
+        rounds ++;
+        computerScore ++;
+        resultsSelector.textContent = 'You chose rock and the computer chose paper. You lose!';
+        scoreRounds();
         return "You lose";
     }
 
     else if (lowerCasePlayerInput === 'rock' && getComputerChoice === 'Scissors'){
         score ++;
+        rounds ++;
+        //console.log('You win');
+        resultsSelector.textContent = 'You chose rock and the computer chose scissors. You win!';
+        scoreRounds();
         return "You win"; 
     }
 
     else if (lowerCasePlayerInput === 'paper' && getComputerChoice === 'Rock'){
         score ++;
+        //console.log('You win');
+        resultsSelector.textContent = 'You chose paper and the computer chose rock. You win!';
+        scoreRounds();
         return 'You win'; 
     }
 
     else if (lowerCasePlayerInput === 'paper' && getComputerChoice === 'Paper'){
+        //console.log('Draw');
+        rounds ++;
+        resultsSelector.textContent = 'You chose paper and the computer chose paper. Draw!';
+        scoreRounds();
         return 'Draw';
     }
 
     else if (lowerCasePlayerInput === 'paper' && getComputerChoice === 'Scissors'){
+        //console.log('You lose');
+        rounds ++;
+        computerScore ++;
+        resultsSelector.textContent = 'You chose paper and the computer chose scissors. You lose!';
+        scoreRounds();
         return 'You lose';
     }
 
     else if (lowerCasePlayerInput === 'scissors' && getComputerChoice === 'Rock'){
+        //console.log('You lose');
+        rounds ++;
+        computerScore ++;
+        resultsSelector.textContent = 'You chose scissors and the computer chose rock. You lose!';
+        scoreRounds();
         return 'You lose';
     }
 
     else if (lowerCasePlayerInput === 'scissors' && getComputerChoice === 'Paper'){
         score ++;
+        rounds ++;
+        //console.log('You win');
+        resultsSelector.textContent = 'You chose scissors and the computer chose paper. You win!';
+        scoreRounds();
         return 'You win';
     }
 
     else if (lowerCasePlayerInput === 'scissors' && getComputerChoice === 'Scissors'){
+        //console.log('Draw');
+        rounds ++;
+        resultsSelector.textContent = 'You chose scissors and the computer chose scissors. Draw!';
+        scoreRounds();
         return 'Draw';
     }
 
@@ -83,28 +143,47 @@ function playRound(lowerCasePlayerInput, getComputerChoice){
     }
 }
 
-console.log(playRound(lowerCasePlayerInput, getComputerChoice()));
-console.log('You won ' + score + ' rounds');
+
+
+//console.log(playRound(lowerCasePlayerInput, getComputerChoice()));
+//console.log('You won ' + score + ' rounds');
 
 
 
-//Write a new function called game()
-//Loop it 5 times
-//Call the playRound inside game()
-//Play 5 rounds by repeating playRound function 5 times
-//Keeps score and reports a winner or loser at the end 
-//Loop the playRound function inside game()
 
-function game(){
 
-    for (let i = 0; i < 4; i++)
-    {
-        playerInput = prompt("Please, select one of these: rock, paper or scissor.")
-        lowerCasePlayerInput;
-        console.log(playRound(lowerCasePlayerInput, getComputerChoice()));
-        console.log('You won ' + score + ' rounds');
-    }
-    
-}
+const rockSelector = document.querySelector('#rock');
 
-game();
+//console.log(rockSelector);
+
+rockSelector.addEventListener('click', () => playRound('rock', getComputerChoice()));
+
+const paperSelector = document.querySelector('#paper');
+
+//console.log(paperSelector);
+
+paperSelector.addEventListener('click', () => playRound('paper', getComputerChoice()));
+
+const scissorsSelector = document.querySelector('#scissors');
+
+//console.log(scissorsSelector);
+
+scissorsSelector.addEventListener('click', () => playRound('scissors', getComputerChoice()));
+
+const bodySelect = document.querySelector('body');
+
+//console.log(bodySelect);
+
+const resultsSelector = document.querySelector('#results');
+
+//console.log(resultsSelector);
+
+const scoreSelector = document.querySelector('#score');
+
+const winnerSelector = document.querySelector('#winner');
+
+//console.log(winnerSelector);
+
+const matchesPlayed = document.querySelector('#matches');
+//matchesPlayed.style.color = 'red';
+//console.log(matchesPlayed);
